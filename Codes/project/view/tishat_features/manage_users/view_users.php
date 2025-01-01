f770e2f5acc63c9866e7a3cf51f72cfeb2505906
Towsif <towsif1528@gmail.com>
2025-01-02 00:39:23 +0600

    uploaded updated codes

--- Codes/project/view/tishat_features/manage_users/view_users.php
+++ Codes/project/view/tishat_features/manage_users/view_users.php
@@ -12,47 +12,144 @@
     // }
 ?>
 <html>
+<html>
 <head>
     <title>View Users Page</title>
     <link rel="stylesheet" href="view_users.css">
+    <link rel="icon" type="image/x-icon" href="../../../asset/images/logo/ad.svg">
 </head>
 <body>
     <div class="main_container">
         <div class="container">
             <div class="header">
+                <div class="page_name">
+                    <img src="../../../asset/images/ad.png" alt="">
+                    <p>View Users Page</p>
+                </div>
+                <div class="admin_name">
+                    <div class="image_container">
+                        <img src="../../../asset/images/TOWSIF_PIC.jpg" alt="">
+                    </div>
+                    <p>Hello Admin</p>
+                </div>
             </div>
             <div class="left">
+                <div class="logo_cotainer">
+                    <img src="../../../SVG/ad.svg" alt="">
+                    <img src="../../../SVG//adventure.svg" alt="">
+                </div>
                 <div class="admin_menu_container">
-                    <button>Dashboard</button>
-                    <button>Manage ADs</button>
-                    <button>View User</button>
-                    <button>Manage Users</button>
-                    <button>View Advertiesers</button>
-                    <button>Manage ADvertisers</button>
-                    <button>AD Analytics </button>
-                    <button>System settings</button>
-                    <button>Flagged Content</button>
-                    <button>Generate Report </button>
-                    <button>Change Password </button>
+                    <div>
+                        <img src="../../../asset/images/dashboard.png" alt="">
+                        <button>Dashboard</button>
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/ad.png" alt="">
+                        <button>Manage ADs</button>
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/users.png" alt="">
+                        <button>View User</button>
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/manage_users.png" alt="">
+                        <button>Manage Users</button>
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/advs.png" alt="">
+                        <button>View Advertiesers</button>
+
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/manage_advs.png" alt="">
+                        <button>Manage ADvertisers</button>
+
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/analytics.png" alt="">
+                        <button>AD Analytics </button>
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/settings.png" alt="">
+                        <button>System settings</button>
+
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/flag.png" alt="">
+                        <button>Flagged Content</button>
+
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/report.png" alt="">
+                        <button>Generate Report </button>
+
+                    </div>
+                    <div>
+                        <img src="../../../asset/images/change_pass.png" alt="">
+                        <button>Change Password </button>
+
+                    </div>
+                    <div class="go_back">
+                        <button>
+                        <img src="../../../asset/images/back.png" alt="">
+                        <a href="../../dashboard/admin_menu.php?id=<?php echo $idd ?>"> Go Back</a> </button>
+                    </div>
                 </div>
             </div>
-            <div class="right"></div>
+            
             <div class="middle">
-                <div class="user_manage_contatiner">
-                    <p>search Users</p>
-                    <form action="">
-                        <input type="text">
-                        <button>search</button>
-                        <div class="user_status_container">
-                            <div class="active_users">Active users</div>
-                            <div class="inactive_users">Inactive users</div>
+                <form action="">
+                    <div class="user_manage_contatiner">
+                        <div class="search_container">
+                            <div class="search_user_container">
+                                <img src="../../../asset/images/search.png" alt="">
+                                <p>Search User</p>
+                            </div>
+                            <div class="search_input_container">
+                                <input type="text">
+                                <button>search</button>
+                            </div>
                         </div>
-                    </form>
-                    <button>User Activity History </button><br>
-                    <button>Manage Status</button><br>
-                    <button>Edit Information </button>
-                </div>
-                <div class='all_user'>
+
+                        <div class="user_status_container">
+                           
+                                <button>
+                                    <a href="">Active Users</a>
+                                </button>
+                          
+                            
+                                <button>
+                                    <a href="">Inactive Users</a>
+                                </button>
+                           
+                        </div>
+
+                        <div class="user_activity_container">
+                        <button> 
+                            <a href="">User Activity History</a>
+                         </button>
+
+                        </div>
+
+                        <div class="manupulate_container">
+                        
+                            <button>
+                                <a href="">
+                                Manage Status
+                                </a>
+                            </button>
+                            
+                            <button>
+                            <a href="">
+                            Edit Information
+                            </a> </button>
+                            
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="right">
+                <div class='all_user_container'>
                 <table border=1 cellspacing="0" align="center" width="50%">
             <tr align="center">
                 <th>ID</th>
@@ -75,18 +172,17 @@
                 <td><?php echo $row['email']; ?></td>
                 <td><?php echo $row['type']; ?></td>
                 <td>
-                    <a href="edit.php?id=<?php echo $idd?>&idt=<?php echo $row['id']?>"> EDIT </a> 
+                    <button>
+                        <a href="edit.php?id=<?php echo $idd?>&idt=<?php echo $row['id']?>"> EDIT </a> 
+                    </button>
                 </td>
                 <td>
-                <a href="../model/delete_user.php?id=<?php echo $idd?>&idt=<?php echo $row['id']?>"> DELETE </a> 
+                    <button>
+                        <a href="../model/delete_user.php?id=<?php echo $idd?>&idt=<?php echo $row['id']?>"> DELETE </a> 
+                    </button>
                 </td>
                 <?php }} ?>
             </tr>
-                <tr align="center">
-                <td colspan="6">
-                    <a href="../../dashboard/admin_menu.php?id=<?php echo $idd ?>"> Go to Menu </a> 
-                </td>
-                </tr>
                 </table>
                 </div>
             </div>
